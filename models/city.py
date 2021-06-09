2a64ddb4eae1e01a2d7e753b59c7f65378222a80
Grace Fallon <graceleefallon@gmail.com>
2021-06-09 20:12:25 +0000

    Incorporating abstracted dbstorage tools across classes

--- models/city.py
+++ models/city.py
@@ -3,9 +3,12 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from models.state import State
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     state_id = Column(String(60), ForeignKey(State.id), nullable=False)
     name = Column(String(128), nullable=False)
     __tablename__ = "cities"
+
+    places = relationship("Place", cascade="all, delete-orphan", backref='cities')   
