93cd28b83b387630adb7060443d819a325b8e075
Grace Fallon <graceleefallon@gmail.com>
2021-06-11 22:21:01 +0000

    Deploy front end

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Ship front end bundle to our server """
+import os.path
+from fabric.api import local, env, put, run
+
+
+env.hosts = ['54.152.238.19']
+
+
+def do_deploy(archive_path):
+    """ Deploy sit to server index """
+    
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archiveName = archive_path[9:]
+        archiveNameWithoutExtension = archiveName[:-4]
+
+        put(archive_path, '/tmp/' + archiveName)
+        run('mkdir -p /data/web_static/releases/' + archiveNameWithoutExtension)
+        run('tar -xzvf /tmp' + archiveName + ' -C ' + '/data/web_static/releases/' + archiveNameWithoutExtension + ' --strip-components=1')
+        run('rm -f /tmp/' + archiveName)
+        run('rm -f /data/web_static/current')
+        run('ln -sf /data/web_static/releases/' + archiveNameWithoutExtension + ' /data/web_static/current')
+        local('rm -r ' + archive_path)
+        local('rm /data/web_static/current')
+        return True
+    except:
+        return False
+
+def do_pack():
+    """ Pack up the front end """
+    now = datetime.now()
+
+    tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+    tarArchivePath = "versions/" + tarArchiveName
+
+    local("mkdir -p versions")
+    local("tar -czvf " + tarArchivePath + " web_static")        
