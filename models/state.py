ecbfde6e348242f86c3fc6ab033e036c2e615b12
HLozano12 <HLR8503@gmail.com>
2021-06-14 07:55:31 +0000

    pep8

--- models/state.py
+++ models/state.py
@@ -5,17 +5,19 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
-
 class State(BaseModel, Base):
     """ State class """
-    name = Column(String(128), nullable=False)    
+    name = Column(String(128), nullable=False)
     __tablename__ = 'states'
-    cities = relationship('City', cascade='all, delete-orphan', backref='state')
+    cities = relationship(
+        'City',
+        cascade='all, delete-orphan',
+        backref='state')
 
     @property
     def cities(self):
         """ Returns list of city instances, connected to state id """
-        #ORM code to retrieve from table
+        # ORM code to retrieve from table
         from models import storage
         list_of_cities = []
         for instance in storage.all(City).values():
--- models/user.py
+++ models/user.py
@@ -9,9 +9,17 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String (128))
-    last_name = Column(String (128))
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     __tablename__ = 'users'
 
-    places = relationship("Place", cascade="all, delete-orphan", backref='user')
-    reviews = relationship("Review", cascade="all, delete-orphan", backref='user')
+    places = relationship(
+        "Place",
+        cascade="all,
+        delete-orphan",
+        backref='user')
+    reviews = relationship(
+        "Review",
+        cascade="all,
+        delete-orphan",
+        backref='user')
