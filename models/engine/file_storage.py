fa7dcd48f461043550fc47ae2381c4c0af00f4f3
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-18 20:54:51 +0000

    Updating Pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,7 +63,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = FileStorage.classes
-                                          [val['__class__']](**val)
+                        self.all()[key] = FileStorage.classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
