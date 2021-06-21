31ed7dcbc201f21d198cdd47be2791a8f01d8d65
angelofgrace <graceleefallon@gmail.com>
2021-06-21 23:14:58 +0000

    Include .html extension in reference to template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def numeral(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbered_template(n):
-    return render_template('5-number', number=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
