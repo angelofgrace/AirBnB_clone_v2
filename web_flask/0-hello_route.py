a9c068bb145514b668aed89f77cb344303b92a53
angelofgrace <graceleefallon@gmail.com>
2021-06-21 15:35:07 +0000

    pep8 styling

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Start a Flask web app """
 
+
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-	return 'Hello HBNB!'
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
