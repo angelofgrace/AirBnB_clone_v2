38527437398d35a6be921af11fca5dad7ecec82d
angelofgrace <graceleefallon@gmail.com>
2021-06-21 23:28:50 +0000

    Output html conditional of variable value in python

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Start a Flask web app """
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def title():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_string(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<string:text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_string(text='is cool'):
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numeral(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbered_template(n):
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_even(n):
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run()
