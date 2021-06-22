16e96fd0fbac012b1a8d01fdbba26d34293467be
angelofgrace <graceleefallon@gmail.com>
2021-06-22 00:26:06 +0000

    Use remove() to end threaded session, handle instance retriaval for states in filestorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,4 +71,4 @@ class DBStorage():
 
     def close(self):
         #ending the session in mySQL
-        self.session.close()
+        self.session.remove()
--- models/state.py
+++ models/state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models import storage
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
 
 
 class State(BaseModel, Base):
@@ -15,10 +16,12 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         """ Returns list of city instances, connected to state id """
+        if getenv('HBNB_TYPE_STORAGE') == 'db':
         #ORM code to retrieve from table
-        from models import storage
-        list_of_cities = []
-        for instance in storage.all(City).values():
-            if instance.state_id == self.id:
-                list_of_cities.append(instance)
-        return list_of_cities
+            list_of_cities = []
+            for instance in storage.all(City).values():
+                if instance.state_id == self.id:
+                    list_of_cities.append(instance)
+            return list_of_cities
+	else:
+            return storage.all(State)
